4506f77825a9aa9c1f63f4c1999f9b988d3809f8
cruzbeymar74 <cruzbeymar74@gmail.com>
2025-06-02 12:39:47 +0000

    correcion match

--- src/app/match/molecules/index.ts
+++ src/app/match/molecules/index.ts
@@ -1,4 +1,4 @@
-export { StatCard } from './StatCard';
+export { StatCard } from './StatCard.tsx';
 export { FilterSelect } from './FilterSelect';
 export { PetMiniCard } from './PetMiniCard';
 export { ActionControls } from './ActionControls';
