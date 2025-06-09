5b2235463a9f43ee110122d86b5a2c9b6a829483
Geraldiene Lavadenz <riomahabramromamateo@gmail.com>
2025-06-09 00:20:04 -0400

    misFavoritos

---
+++ src/app/mis-favoritas/page.tsx
@@ -0,0 +1,8 @@
+// src/app/mis-favoritas/page.tsx
+'use client'
+
+import MascotasFavoritas from '@/components/MascotasFavoritas'
+
+export default function PaginaFavoritas() {
+  return <MascotasFavoritas />
+}
